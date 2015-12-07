7b01b74c3b252045ccfb500e674a33e345a94ab1
Petr Kuška <kuska.petr@gmail.com>
2015-12-07 02:01:08 +0100

    Update Day 1, part 2

--- src/advent/Day1.java
+++ src/advent/Day1.java
@@ -14,10 +14,15 @@ public class Day1 {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int left = 0, right = 0;
         String line = in.readLine();
+        boolean second_part = true;
         for (int i = 0; i < line.length(); i++) {
             char c = line.charAt(i);
             if (c == '(') left++;
             if (c == ')') right++;
+            if (second_part && left - right < 0) {
+                System.out.println("Second part: "+i+1);
+                second_part = false;
+            }
         }
         System.out.println(left - right);
     }
