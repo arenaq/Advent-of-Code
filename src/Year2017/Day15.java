c3f89d7f527efc1805a5b6ff1ac82e191d746918
Petr Kuška <kuska.petr@gmail.com>
2018-12-05 21:58:05 +0100

    tmp

---
+++ src/Year2017/Day15.java
@@ -0,0 +1,11 @@
+package Year2017;
+
+/**
+ * Created by arenaq on 19.12.2017.
+ */
+public class Day15 {
+    public static void main(String[] args) {
+        int genA = 634;
+        int genB = 301;
+    }
+}
