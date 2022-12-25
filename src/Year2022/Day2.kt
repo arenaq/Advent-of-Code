9c3b1f5aa219e95c50e4264cf206ef772a0bd7a7
Petr Kuška <kuska.petr@gmail.com>
2022-12-25 02:04:58 +0100

    Add solution for Day 2 - part 2

--- src/Year2022/Day2.kt
+++ src/Year2022/Day2.kt
@@ -19,4 +19,21 @@ fun main() {
         }
     }
     println(score)
+    // part 2
+    score = 0
+    rounds.forEach { round ->
+        score += when (round) {
+            "A X" -> 3 // 0 + 3
+            "A Y" -> 4 // 3 + 1
+            "A Z" -> 8 // 6 + 2
+            "B X" -> 1 // 0 + 1
+            "B Y" -> 5 // 3 + 2
+            "B Z" -> 9 // 6 + 3
+            "C X" -> 2 // 0 + 2
+            "C Y" -> 6 // 3 + 3
+            "C Z" -> 7 // 6 + 1
+            else -> 0
+        }
+    }
+    println(score)
 }
